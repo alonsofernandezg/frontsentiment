cd09120c02d7d0c67f447c690580273f72c73964
alonsofernandezg <79119741+alonsofernandezg@users.noreply.github.com>
2022-09-02 13:22:25 -0600

    Add files via upload

---
+++ js/controlador_inicio.js
@@ -0,0 +1,77 @@
+'use strict';
+
+
+
+/*------------------------------------------------------*/
+const botonAnalizar= document.querySelector('#btnAnalizar');
+
+const inputDescripcion = document.querySelector('#texto');
+
+
+let validar= () =>{
+  let error =false; 
+  
+  let elementos_requeridos = document.querySelectorAll('#frm-analizar [required]');
+     
+  
+  for (let i = 0; i < elementos_requeridos.length; i++) {
+      if (elementos_requeridos[i].value == '') {
+          elementos_requeridos[i].classList.add('input-error');
+          error = true;
+      } else {
+          elementos_requeridos[i].classList.remove('input-error');
+      }
+  }
+
+  
+  return error;
+};
+
+
+function limpiar(){
+    
+  
+  inputDescripcion.value ='';
+  
+};
+
+function obtenerDatos(){
+  let errorv = validar();
+ 
+  if (errorv) {
+       
+      Swal.fire({
+          'title':'Sus datos no se pudieron enviar',
+          'text':'Por favor complete todos los campos',
+          'icon':'warning'
+      });
+  } else {
+      
+      
+   
+      let Descripcion = inputDescripcion.value;
+      
+
+      
+      analizar_texto( Descripcion );  
+     
+  
+
+      Swal.fire({
+          'title': 'Proceso realizado con éxito',
+          'text': 'Sus datos se enviaron adecuadamente',
+          'icon': 'success'
+      }).then(() => {
+          limpiar();
+      });
+  }
+};
+
+/*carga respuesta*/
+
+let resultado = analizar_texto()
+document.getElementById("resultado").innerHTML = JSON.stringify(resultado, null, 4);
+
+
+
+botonRegistrar.addEventListener('click',obtenerDatos);
---
+++ js/servicio_inicio.js
@@ -0,0 +1,24 @@
+'use strict'
+
+  
+ 
+let analizar_texto = async(pdescripcion) => {
+    let  resultado= [];
+
+    await axios({
+        method: 'get',
+        url: 'https://sentimenttextapp.azurewebsites.net/index',
+        responseType: 'json',
+        data: {
+            
+            'texto':pdescripcion,
+            
+        } 
+    }).then((res) => {
+        resultado = res.data
+    }).catch((err) => {
+        console.log('No se pudo establecer la comunicación con el servidor, ocurrió el siguiente error: ', err)
+    });
+
+    return resultado;
+};
