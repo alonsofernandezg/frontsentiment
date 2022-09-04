505156765e004f4c5f49301d1fb265f325295dcd
alonsofernandezg <79119741+alonsofernandezg@users.noreply.github.com>
2022-09-04 14:45:33 -0600

    Add files via upload

---
+++ controlador_inicio.js
@@ -0,0 +1,94 @@
+'use strict';
+
+
+
+/*------------------------------------------------------*/
+const botonAnalizar= document.querySelector('#btnAnalizar');
+
+const inputDescripcion = document.querySelector('#texto');
+
+
+let validar= () =>{
+  let error =false; 
+  
+  let elementos_requeridos = document.querySelectorAll('#frm-analizar [required]');
+     
+  
+  for (let i = 0; i < elementos_requeridos.length; i++) {
+      if (elementos_requeridos[i].value == '') {
+          elementos_requeridos[i].classList.add('input-error');
+          error = true;
+      } else {
+          elementos_requeridos[i].classList.remove('input-error');
+      }
+  }
+
+  
+  return error;
+};
+
+
+function limpiar(){
+    
+  
+  inputDescripcion.value ='';
+  
+};
+
+function obtenerDatos(){
+  let errorv = validar();
+ 
+  if (errorv) {
+       
+      Swal.fire({
+          'title':'Sus datos no se pudieron enviar',
+          'text':'Por favor complete todos los campos',
+          'icon':'warning'
+      });
+  } else {
+      
+      
+   
+      let Descripcion = inputDescripcion.value;
+      
+
+      
+      analizar_texto( Descripcion );  
+     
+  
+
+      Swal.fire({
+          'title': 'Proceso realizado con éxito',
+          'text': 'Sus datos se enviaron adecuadamente',
+          'icon': 'success'
+      }).then(() => {
+          limpiar();
+      });
+  }
+};
+
+/*carga respuesta*/
+const tbody = document.getElementById('#tbl-resultado tbody');
+let lista_resultados = [];
+let mostrar_resultados = async() => {
+    let lista_resultados = await analizar_texto();
+    
+  
+    for (let i = 0; i < lista_resultados.length; i++) {
+        let fila = tbody.insertRow();
+      
+       let celda_oracion = fila.insertCell().innerHTML = lista_resultados[i]['sentence'];
+       let celda_tipo_incidente = fila.insertCell().innerHTML = lista_incidente[i]['probas'];
+       let celda_ruta = fila.insertCell().innerHTML = lista_incidente[i]['is_multilabel'];
+       let celda_ubicacion_incidente =  fila.insertCell().innerHTML = lista_incidente[i]['output'];
+   
+      
+    }
+
+   
+};
+
+
+
+mostrar_resultados()
+botonAnalizar.addEventListener('click',obtenerDatos);
